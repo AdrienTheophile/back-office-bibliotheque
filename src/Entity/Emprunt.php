7333e370211b2132f62791e9862905344aa9fe79
alexian-a <alexian.arg11@gmail.com>
2026-02-10 17:10:28 +0100

    ajout des entites Adherent et Emprunt

---
+++ src/Entity/Emprunt.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EmpruntRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EmpruntRepository::class)]
+class Emprunt
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $idEmp = null;
+
+    #[ORM\Column]
+    private ?\DateTime $dateEmprunt = null;
+
+    #[ORM\Column]
+    private ?\DateTime $dateRetour = null;
+
+    public function getId(): ?int
+    {
+        return $this->idEmp;
+    }
+
+    public function getDateEmprunt(): ?\DateTime
+    {
+        return $this->dateEmprunt;
+    }
+
+    public function setDateEmprunt(\DateTime $dateEmprunt): static
+    {
+        $this->dateEmprunt = $dateEmprunt;
+
+        return $this;
+    }
+
+    public function getDateRetour(): ?\DateTime
+    {
+        return $this->dateRetour;
+    }
+
+    public function setDateRetour(\DateTime $dateRetour): static
+    {
+        $this->dateRetour = $dateRetour;
+
+        return $this;
+    }
+}
