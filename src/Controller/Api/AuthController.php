11354c03678d55855f8fb6853cb0aa23ae5e8f12
alexian-a <alexian.arg11@gmail.com>
2026-03-10 15:51:54 +0100

    modification de l'api auth

--- src/Controller/Api/AuthController.php
+++ src/Controller/Api/AuthController.php
@@ -10,28 +10,33 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route('/api')]
 class AuthController extends AbstractController
 {
-    #[Route('/login_check', name: 'api_login_check', methods: ['POST'])]
+    #[Route('/login', name: 'api_login_check', methods: ['POST'])]
     public function login(): JsonResponse
     {
         return $this->json(['message' => 'Missing credentials'], 401);
     }
-
+    
     #[Route('/user/me', name: 'api_user_me', methods: ['GET'])]
     public function me(): JsonResponse
     {
         /** @var Utilisateur|null $user */
         $user = $this->getUser();
-
         if (!$user) {
             return $this->json(['message' => 'Not authenticated'], 401);
         }
-
+        $adherent = $user->getAdherent(); // récupère le profil Adherent
         return $this->json([
-            'id' => $user->getId(),
-            'email' => $user->getEmail(),
-            'nom' => $user->getNom(),
-            'prenom' => $user->getPrenom(),
-            'roles' => $user->getRoles(),
+            'id'        => $user->getId(),
+            'email'     => $user->getEmail(),
+            'nom'       => $user->getNom(),
+            'prenom'    => $user->getPrenom(),
+            'roles'     => $user->getRoles(),
+            'adherent'  => $adherent ? [
+                'id'            => $adherent->getIdAdh(),
+                'dateAdhesion'  => $adherent->getDateAdhesion()?->format('Y-m-d'),
+                'numTel'        => $adherent->getNumTel(),
+                'estActif'      => $adherent->isEstActif(),
+            ] : null,
         ]);
     }
 }
